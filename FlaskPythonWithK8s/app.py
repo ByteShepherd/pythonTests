0fa88cfdfd8402f2166c1ff5cc324902603d631e
byteshepherd <tuscho@gmail.com>
2024-11-08 09:34:02 -0300

    ...

--- FlaskPythonWithK8s/app.py
+++ FlaskPythonWithK8s/app.py
@@ -1,10 +1,13 @@
 from flask import request, Flask
 import json
+import os
 
 app1 = Flask(__name__)
 @app1.route('/')
 def hello_world():
-    return 'Olá mundo, POD :) '
+    pod_name = os.environ['POD_NAME']
+    pod_ip = os.environ['POD_IP']
+    return 'Olá POD:' + pod_name + ' IP:' + pod_ip
 
 if __name__ == '__main__':
     app1.run(debug=True, host='0.0.0.0')
